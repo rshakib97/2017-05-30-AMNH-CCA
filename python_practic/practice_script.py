2f46b9180843f19b2fadf31e0c5323ce25a469c2
Rayhan Shakib <Rayhans1210@gmail.com>
2017-05-31 15:39:34 -0400

    added a sample class to play with called algebra in practice_script.py

--- python_practic/practice_script.py
+++ python_practic/practice_script.py
@@ -2,8 +2,36 @@
 little adding
 this script contains an example class that we will write. the class will be called algebra, and it will contain some simple methods to do some algebraic manipulation.
 
-this script will end with a sample way to call
+this script will end with a sample way to call and use the class.
 
 
 
 '''
+sample_int = 5
+class algebra:
+
+    def __init__(self,x,y):
+        # we are defining the arguments as attributes of
+        # the object itself.
+        self.x = x
+        self.y = y
+        
+    def add( self ) :
+        return self.x + self.y
+
+
+    def subtract( self ) :
+        return self.x - self.y
+
+    def divide( self ) :
+        return self.x/self.y
+
+    def multiply( self ) :
+        return self.x* self.y
+
+  
+    def complicated(self):
+        return self.multiply() + self.add()
+
+    def modify_x(self):
+        self.x = self.x *2
