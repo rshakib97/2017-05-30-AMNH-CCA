c063581e38f6a70cae315fb855bfaf424fbc346f
Rayhan Shakib <Rayhans1210@gmail.com>
2017-05-31 11:26:20 -0400

    added the py to the git

--- python_practic/practice_script.py
+++ python_practic/practice_script.py
@@ -1,4 +1,5 @@
 '''
+little adding
 this script contains an example class that we will write. the class will be called algebra, and it will contain some simple methods to do some algebraic manipulation.
 
 this script will end with a sample way to call
